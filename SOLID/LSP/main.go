a12d50f084761a7cccfe0befbc116c6d267dd5ea
aquezada-ext <aquezada@externo.auna.pe>
2024-02-16 13:52:21 -0500

    refactor: :hammer: update some principles

--- SOLID/LSP/main.go
+++ SOLID/LSP/main.go
@@ -9,29 +9,81 @@ package main
 
 import "fmt"
 
-type area interface {
-	getArea() int
+type Sized interface {
+	GetWidth() int
+	SetWidth(width int)
+	GetHeight() int
+	SetHeight(height int)
 }
+
 type Rectangle struct {
-	width  int
-	height int
+	width, height int
 }
 
+// vvv !! POINTER
+func (r *Rectangle) GetWidth() int {
+	return r.width
+}
+
+func (r *Rectangle) SetWidth(width int) {
+	r.width = width
+}
+
+func (r *Rectangle) GetHeight() int {
+	return r.height
+}
+
+func (r *Rectangle) SetHeight(height int) {
+	r.height = height
+}
+
+// modified LSP
+// If a function takes an interface and
+// works with a type T that implements this
+// interface, any structure that aggregates T
+// should also be usable in that function.
 type Square struct {
-	value int
+	Rectangle
 }
 
-func (r *Rectangle) getArea() int {
-	return r.width * r.height
+func NewSquare(size int) *Square {
+	sq := Square{}
+	sq.width = size
+	sq.height = size
+	return &sq
 }
 
-func (s *Square) getArea() int {
-	return s.value * s.value
+func (s *Square) SetWidth(width int) {
+	s.width = width
+	s.height = width
+}
+
+func (s *Square) SetHeight(height int) {
+	s.width = height
+	s.height = height
+}
+
+type Square2 struct {
+	size int
+}
+
+func (s *Square2) Rectangle() Rectangle {
+	return Rectangle{s.size, s.size}
+}
+
+func UseIt(sized Sized) {
+	width := sized.GetWidth()
+	sized.SetHeight(10)
+	expectedArea := 10 * width
+	actualArea := sized.GetWidth() * sized.GetHeight()
+	fmt.Print("Expected an area of ", expectedArea,
+		", but got ", actualArea, "\n")
 }
 
 func main() {
-	rectangle := Rectangle{4, 5}
-	square := Square{5}
-	fmt.Println(rectangle.getArea())
-	fmt.Println(square.getArea())
+	rc := &Rectangle{2, 3}
+	UseIt(rc)
+
+	sq := NewSquare(5)
+	UseIt(sq)
 }
--- SOLID/SRP/main.go
+++ SOLID/SRP/main.go
@@ -5,41 +5,84 @@ menos flexible, más rígido y, en definitiva, menos tolerante al cambio.
 El principio de responsabilidad única no se basa en crear clases con un solo método,
 sino en diseñar componentes que solo estén expuestos a una fuente de cambio
 */
+
 package main
 
 import (
 	"fmt"
-
-	"github.com/ArthurQR98/patterns_design_go/SOLID/SRP/notification"
-	"github.com/ArthurQR98/patterns_design_go/SOLID/SRP/repository"
+	"log"
+	"net/url"
+	"os"
+	"strings"
 )
 
-type UseCase struct {
-	repo     repository.Repo
-	notifier notification.Notifier
+var entryCount = 0
+
+type Journal struct {
+	entries []string
 }
 
-func NewUseCase(repo repository.Repo, notifier notification.Notifier) UseCase {
-	return UseCase{repo, notifier}
+func (j *Journal) String() string {
+	return strings.Join(j.entries, "\n")
 }
 
-func (u *UseCase) doSomethingWithTaxes() {
-	fmt.Println("Do something related with taxes ...")
+func (j *Journal) AddEntry(text string) int {
+	entryCount++
+	entry := fmt.Sprintf("%d: %s", entryCount, text)
+	j.entries = append(j.entries, entry)
+	return entryCount
 }
 
-func (u *UseCase) saveChange() {
-	u.repo.Update()
+func (j *Journal) RemoveEntry(index int) {}
+
+// separation of concerns
+// God Object
+
+func (j *Journal) Save(filename string) {
+	err := os.WriteFile(filename, []byte(j.String()), 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-func (u *UseCase) notify() {
-	u.notifier.Notify("Te esperamos en la fiesta!!")
+func (j *Journal) Load(filename string) {}
+
+func (j *Journal) LoadFromWeb(url *url.URL) {}
+
+var LineSeparator = "\n"
+
+func SaveToFile(j *Journal, filename string) {
+	err := os.WriteFile(filename, []byte(strings.Join(j.entries, LineSeparator)), 0664)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type Persistence struct {
+	lineSeparator string
+}
+
+func (p *Persistence) SaveToFile(j *Journal, filename string) {
+	err := os.WriteFile(filename, []byte(strings.Join(j.entries, p.lineSeparator)), 0664)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
-	repo := repository.Repo{}
-	notify := notification.Notifier{}
-	myUseCase := NewUseCase(repo, notify)
-	myUseCase.doSomethingWithTaxes()
-	myUseCase.notify()
-	myUseCase.saveChange()
+	j := Journal{}
+	j.AddEntry("I cried today")
+	j.AddEntry("I ate a bug")
+
+	fmt.Println(j.String())
+
+	// Bad
+	j.Save("bad.txt")
+
+	// Good
+	SaveToFile(&j, "journal.txt")
+
+	// Good
+	p := Persistence{lineSeparator: "\r\n"}
+	p.SaveToFile(&j, "journal.txt")
 }
